86c32b52b41507a96a04610f27f938ce26ae6e6f
Mathieu Doyon <mathieu.doyon@ssense.com>
2017-03-30 14:50:03 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# SQS Push
+
+simple cli command that push STDIN to your AWS SQS queue
+
+---
+
+## Usage
+
+```bash
+# login to AWS
+sqspush login --key=aws_access_key_id --secret=aws_secret_access_key
+
+# Send payload.json to your queue
+cat ./mock/payload.json | sqspush --queue=https://sqs.us-west-2.amazonaws.com/XXYYYZZZZXXX/queue-name.fifo --region=us-west-2 --group=mygroup
+
+# or use echo to send data
+echo '{ "foo": "bar" }' | sqspush --queue=https://sqs.us-west-2.amazonaws.com/XXYYYZZZZXXX/queue-name --region=us-west-2 --group=mygroup
+
+# Getting help
+sqspush --help
+# NAME:
+#    SQS PUSH - push stdin into sqs queue
+# USAGE:
+#    cat ./payload.json | sqspush [global options] command [command options] [arguments...]
+#
+# COMMANDS:
+#    login, l  login aws credentials
+#    help, h   Shows a list of commands or help for one command
+#
+# GLOBAL OPTIONS:
+#    --cred-path value, -c value  AWS credentials path (default: "~/.aws/credentials")
+#    --queue value, -q value      SQS queue URL
+#    --region value, -r value     SQS queue REGION
+#    --profile value, -p value    AWS login profile
+#    --retries value              SQS queue retries (default: 2)
+#    --group value, -g value      SQS queue MessageGroupId (default: "5ePlmiO61JA5iqU1jBkjLYv2Xp4=")
+#    --help, -h                   show help
+#    --version, -v                print the version
+```
+
+### Configuring Credentials
+Before using the SQS PUSH, ensure that you've configured credentials. The best way to configure credentials on a development machine is to use the `~/.aws/credentials` file, which might look like:
+
+```bash
+[default]
+aws_access_key_id = AKID1234567890
+aws_secret_access_key = MY-SECRET-KEY
+```
+
+or use SQS PUSH login command to create your `~/.aws/credentials`
+
+```bash
+# login to AWS will create or overwrite ~/.aws/credentials file
+sqspush login --key=aws_access_key_id --secret=aws_secret_access_key
+```
---
+++ main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"bufio"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"html/template"
+	"log"
+	"os"
+	"os/user"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/urfave/cli"
+)
+
+const (
+	strlen        = 20
+	loginTemplate = `[default]
+aws_access_key_id={{.Key}}
+aws_secret_access_key={{.Secret}}`
+)
+
+type login struct {
+	Key    string
+	Secret string
+}
+
+func duplicationID() string {
+	b := make([]byte, strlen)
+	rand.Read(b)
+	en := base64.StdEncoding // or URLEncoding
+	d := make([]byte, en.EncodedLen(len(b)))
+	en.Encode(d, b)
+	return string(d)
+}
+
+func main() {
+	// Get user $HOME dir
+	usr, err := user.Current()
+	if err != nil {
+		log.Println("Error getting user home dir", err)
+	}
+
+	app := cli.NewApp()
+	app.Version = "0.2.0"
+	app.Name = "SQS PUSH"
+	app.Usage = "push stdin into sqs queue"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "cred-path, c",
+			Value: usr.HomeDir + "/.aws/credentials",
+			Usage: "AWS credentials path",
+		},
+		cli.StringFlag{
+			Name:  "queue, q",
+			Usage: "SQS queue URL",
+		},
+		cli.StringFlag{
+			Name:  "region, r",
+			Usage: "SQS queue REGION",
+		},
+		cli.StringFlag{
+			Name:  "profile, p",
+			Value: "",
+			Usage: "AWS login profile",
+		},
+		cli.Int64Flag{
+			Name:  "retries",
+			Value: 2,
+			Usage: "SQS queue retries",
+		},
+		cli.StringFlag{
+			Name:  "group, g",
+			Value: duplicationID(),
+			Usage: "SQS queue MessageGroupId",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		fi, err := os.Stdin.Stat()
+		if err != nil {
+			panic(err)
+		}
+		if fi.Mode()&os.ModeNamedPipe == 0 {
+			fmt.Println("no pipe :(")
+		} else {
+			// fmt.Println("hi pipe!")
+			payload := ""
+			s := bufio.NewScanner(os.Stdin)
+			for s.Scan() {
+				payload = payload + s.Text()
+			}
+
+			log.Println("Sending: ", payload)
+
+			sess := session.New(&aws.Config{
+				Region:      aws.String(c.String("region")),
+				Credentials: credentials.NewSharedCredentials(c.String("cred-path"), c.String("profile")),
+				MaxRetries:  aws.Int(c.Int("retries")),
+			})
+
+			svc := sqs.New(sess)
+
+			// Send message
+			sendParams := &sqs.SendMessageInput{
+				MessageBody: aws.String(payload),           // Required
+				QueueUrl:    aws.String(c.String("queue")), // Required
+				// DelaySeconds:           aws.Int64(3),         // (optional) ~ 900s (15 minutes)
+				MessageGroupId:         aws.String(c.String("group")),
+				MessageDeduplicationId: aws.String(duplicationID()),
+			}
+			sendResp, err := svc.SendMessage(sendParams)
+			if err != nil {
+				fmt.Println("err")
+				fmt.Println(err)
+			}
+			fmt.Printf("[Send message] \n%v \n\n", sendResp)
+		}
+
+		return nil
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "login",
+			Aliases: []string{"l"},
+			Usage:   "login aws credentials",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "key",
+					Usage: "aws access key id",
+				},
+				cli.StringFlag{
+					Name:  "secret",
+					Usage: "aws secret access key",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				// Create directory if not exists
+				if _, err := os.Stat(usr.HomeDir + "/.aws"); os.IsNotExist(err) {
+					os.Mkdir(usr.HomeDir+"/.aws", os.ModePerm)
+				}
+
+				f, err := os.Create(usr.HomeDir + "/.aws/credentials")
+				if err != nil {
+					log.Println("create file: ", err)
+					return nil
+				}
+
+				// Writing template
+				t := template.Must(template.New("login").Parse(loginTemplate))
+				err = t.Execute(f, login{
+					Key:    c.String("key"),
+					Secret: c.String("secret"),
+				})
+				if err != nil {
+					log.Println("error writing credentials:", err)
+					return nil
+				}
+
+				fmt.Println("Login Success")
+
+				return nil
+			},
+		},
+	}
+
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   cat ./payload.json | {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+
+	app.Run(os.Args)
+}
